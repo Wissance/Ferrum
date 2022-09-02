b157b3ca22c0d4b522c82b9c1f5216ce6f3eff26
Michael Ushakov <um.nix.user@gmail.com>
2022-09-02 16:11:04 +0500

    keep moving ....

--- api/rest/web_api_handler.go
+++ api/rest/web_api_handler.go
@@ -1,9 +1,16 @@
 package rest
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func (wCtx *WebApiContext) IssueNewToken(respWriter http.ResponseWriter, request *http.Request) {
-
+	vars := mux.Vars(request)
+	realm := vars["realm"]
+	if len(realm) == 0 {
+		// 400
+	}
 }
 
 func (wCtx *WebApiContext) GetUserInfo(respWriter http.ResponseWriter, request *http.Request) {
