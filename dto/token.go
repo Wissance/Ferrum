2ab508ffdcd52bf5616c8ca870f14079adf470af
Michael Ushakov <um.nix.user@gmail.com>
2022-09-05 20:45:15 +0500

    created token_dto structure

--- dto/token.go
+++ dto/token.go
@@ -1,1 +1,12 @@
 package dto
+
+type Token struct {
+	AccessToken     string
+	Expires         int
+	RefreshExpires  int
+	RefreshToken    string
+	TokenType       string
+	NotBeforePolicy int
+	Session         string
+	Scope           string
+}
