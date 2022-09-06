87da95777cdd663624675d440b0244b6660174e8
Michael Ushakov <um.nix.user@gmail.com>
2022-09-06 23:24:46 +0500

    temporary, token stub

---
+++ api/rest/common.go
@@ -0,0 +1,35 @@
+package rest
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// BeforeHandle
+/* This function prepare response headers prior to response handle. It sets content-type and CORS headers.
+ * Parameters:
+ *     - respWriter - gorilla/mux response writer
+ * Returns nothing
+ */
+func beforeHandle(respWriter *http.ResponseWriter) {
+	(*respWriter).Header().Set("Content-Type", "application/json")
+	(*respWriter).Header().Set("Accept", "application/json")
+}
+
+// AfterHandle
+/* This function finalize response handle: serialize (json) and write object and set status code. If error occur during object serialization status code sets to 500
+ * Parameters:
+ *     - respWriter - gorilla/mux response writer
+ *     - statusCode - http response status
+ *     - data - object (json) could be empty
+ * Returns nothing
+ */
+func afterHandle(respWriter *http.ResponseWriter, statusCode int, data interface{}) {
+	(*respWriter).WriteHeader(statusCode)
+	if data != nil {
+		err := json.NewEncoder(*respWriter).Encode(data)
+		if err != nil {
+			(*respWriter).WriteHeader(500)
+		}
+	}
+}
--- api/rest/web_api_handler.go
+++ api/rest/web_api_handler.go
@@ -1,16 +1,25 @@
 package rest
 
 import (
+	"Ferrum/dto"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func (wCtx *WebApiContext) IssueNewToken(respWriter http.ResponseWriter, request *http.Request) {
+	beforeHandle(&respWriter)
 	vars := mux.Vars(request)
 	realm := vars["realm"]
+	var result interface{}
+	status := http.StatusOK
 	if len(realm) == 0 {
 		// 400
+		status = http.StatusBadRequest
+	} else {
+		// todo: validate ...
+		result = dto.Token{AccessToken: "123445"}
 	}
+	afterHandle(&respWriter, status, &result)
 }
 
 func (wCtx *WebApiContext) GetUserInfo(respWriter http.ResponseWriter, request *http.Request) {
