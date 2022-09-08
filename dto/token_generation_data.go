259a9fd538ab2fb963f16f96f69e9aae4517b041
Michael Ushakov <um.nix.user@gmail.com>
2022-09-08 23:06:50 +0500

    it started to pass decode

--- dto/token_generation_data.go
+++ dto/token_generation_data.go
@@ -1,10 +1,10 @@
 package dto
 
 type TokenGenerationData struct {
-	ClientId     string `json:"client_id"`
-	ClientSecret string `json:"client_secret"`
-	GrantType    string `json:"grant_type"`
-	Scope        string `json:"scope"`
-	Username     string `json:"username"`
-	Password     string `json:"password"`
+	ClientId     string `json:"client_id" schema:"client_id"`
+	ClientSecret string `json:"client_secret" schema:"client_secret"`
+	GrantType    string `json:"grant_type" schema:"grant_type"`
+	Scope        string `json:"scope" schema:"scope"`
+	Username     string `json:"username" schema:"username"`
+	Password     string `json:"password" schema:"password"`
 }
