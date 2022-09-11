cfb10c5d783914cfa51b1496d4f35b1faf16892c
Michael Ushakov <um.nix.user@gmail.com>
2022-09-11 23:15:11 +0500

    preliminary method start or update user session impl

--- services/password_based_security.go
+++ services/password_based_security.go
@@ -6,15 +6,16 @@ import (
 	"Ferrum/errors"
 	"Ferrum/managers"
 	"github.com/google/uuid"
+	"time"
 )
 
 type PasswordBasedSecurityService struct {
 	DataProvider *managers.DataContext
-	UserSessions map[uuid.UUID]data.UserSession
+	UserSessions map[string][]data.UserSession
 }
 
 func Create(dataProvider *managers.DataContext) SecurityService {
-	pwdSecService := &PasswordBasedSecurityService{DataProvider: dataProvider, UserSessions: map[uuid.UUID]data.UserSession{}}
+	pwdSecService := &PasswordBasedSecurityService{DataProvider: dataProvider, UserSessions: map[string][]data.UserSession{}}
 	secService := SecurityService(pwdSecService)
 	return secService
 }
@@ -51,7 +52,28 @@ func (service *PasswordBasedSecurityService) CheckCredentials(tokenIssueData *dt
 }
 
 func (service *PasswordBasedSecurityService) StartOrUpdateSession(realm string, userId uuid.UUID, duration int) uuid.UUID {
-	return uuid.UUID{}
+	realmSessions, ok := service.UserSessions[realm]
+	sessionId := uuid.New()
+	// if there are no realm sessions ...
+	if !ok {
+		userSession := data.UserSession{Id: sessionId, UserId: userId, Started: time.Now(),
+			Expired: time.Now().Add(time.Second * time.Duration(duration))}
+		service.UserSessions[realm] = append(realmSessions, userSession)
+		return sessionId
+	}
+	// realm session exists, we should find and update Expired values OR add new
+	for _, s := range realmSessions {
+		if s.UserId == userId {
+			s.Expired = time.Now().Add(time.Second * time.Duration(duration))
+			service.UserSessions[realm] = realmSessions
+			return s.Id
+		}
+	}
+	// such session does not exist, adding
+	userSession := data.UserSession{Id: sessionId, UserId: userId, Started: time.Now(),
+		Expired: time.Now().Add(time.Second * time.Duration(duration))}
+	service.UserSessions[realm] = append(realmSessions, userSession)
+	return userSession.Id
 }
 
 func (service *PasswordBasedSecurityService) GetSession(realm string, userId uuid.UUID) *data.UserSession {
