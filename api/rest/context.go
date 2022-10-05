b55ff0c8ae621358dd0fc5006c7e458a939a6cd0
Michael Ushakov <um.nix.user@gmail.com>
2022-10-05 13:17:02 +0500

    passing logging config to test also

--- api/rest/context.go
+++ api/rest/context.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"Ferrum/logging"
 	"Ferrum/managers"
 	"Ferrum/services"
 )
@@ -9,4 +10,5 @@ type WebApiContext struct {
 	DataProvider   *managers.DataContext
 	Security       *services.SecurityService
 	TokenGenerator *services.JwtGenerator
+	Logger         *logging.AppLogger
 }
