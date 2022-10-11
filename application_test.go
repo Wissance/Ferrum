440e47f6a48886cf12cc0a095bff07c16aa6913f
Michael Ushakov <um.nix.user@gmail.com>
2022-10-11 17:35:56 +0500

    last changes save

--- application_test.go
+++ application_test.go
@@ -26,11 +26,11 @@ var testServerData = data.ServerData{
 				{Name: "testclient1", Type: data.Confidential, Auth: data.Authentication{Type: data.ClientIdAndSecrets,
 					Value: "fb6Z4RsOadVycQoeQiN57xpu8w8wplYz"}},
 			}, Users: []interface{}{
-			map[string]interface{}{"info": map[string]interface{}{"sub": "667ff6a7-3f6b-449b-a217-6fc5d9ac0723",
-				"name": "vano", "preferred_username": "vano",
-				"given_name": "vano ivanov", "family_name": "ivanov", "email_verified": true},
-				"credentials": map[string]interface{}{"password": "1234567890"}},
-		}},
+				map[string]interface{}{"info": map[string]interface{}{"sub": "667ff6a7-3f6b-449b-a217-6fc5d9ac0723",
+					"name": "vano", "preferred_username": "vano",
+					"given_name": "vano ivanov", "family_name": "ivanov", "email_verified": true},
+					"credentials": map[string]interface{}{"password": "1234567890"}},
+			}},
 	},
 }
 var loggingConfig = config.LoggingConfig{Level: "info", Appenders: []config.AppenderConfig{{Level: "info", Type: config.Console},
