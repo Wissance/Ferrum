caac1bfff40ab3ba9b88126e2f41b5876b1d38c1
Michael Ushakov <um.nix.user@gmail.com>
2022-12-18 02:05:26 +0500

    declaring mongodb params + validate method too

--- config/data_source_config.go
+++ config/data_source_config.go
@@ -1,19 +1,32 @@
 package config
 
 type DataSourceType string
+type MongoDbOptions string
 
 const (
 	FILE    DataSourceType = "file"
 	MONGODB DataSourceType = "mongodb"
 )
 
+const ()
+
 // DataSourceConfig represent source where we can get
 /*
  * We attempt to provide config that easily could be used with any datasource:
  * - json file (simplest RO mode)
  * - mongodb (but here we have very simple question how to pass parameters)
+ * Source contains:
+ * 1) if Type is FILE - full path to Json File
+ * 2) if Type is MONGODB - connection string without options, which looks like mongodb://user:password@host:port/
+ * Options are connection options, see - https://www.mongodb.com/docs/drivers/go/current/fundamentals/connection/#std-label-golang-connection-guide
+ * Here we should have Validator too
  */
 type DataSourceConfig struct {
-	Type   DataSourceType `json:"type"`
-	Source string         `json:"source"`
+	Type    DataSourceType    `json:"type"`
+	Source  string            `json:"source"`
+	Options map[string]string `json:"options"`
+}
+
+func (cfg *DataSourceConfig) Validate() error {
+	return nil
 }
