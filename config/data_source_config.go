0c7d55d250580c1e25e66ddade459f120ab2bc12
Michael Ushakov <um.nix.user@gmail.com>
2022-12-18 12:01:27 +0500

    working on a config

--- config/data_source_config.go
+++ config/data_source_config.go
@@ -1,14 +1,42 @@
 package config
 
+import "errors"
+
 type DataSourceType string
-type MongoDbOptions string
+type MongoDbOption string
+type MongoDbOptionValueType string
 
 const (
 	FILE    DataSourceType = "file"
 	MONGODB DataSourceType = "mongodb"
 )
 
-const ()
+const (
+	OperationTimeout       MongoDbOption = "timeoutMS"
+	ConnectionTimeout      MongoDbOption = "connectTimeoutMS"
+	ConnectionsPool        MongoDbOption = "maxPoolSize"
+	ReplicaSet             MongoDbOption = "replicaSet"
+	MaxIdleTime            MongoDbOption = "maxIdleTimeMS"
+	SocketTimeout          MongoDbOption = "socketTimeoutMS"
+	ServerSelectionTimeout MongoDbOption = "serverSelectionTimeoutMS"
+	HeartbeatFrequency     MongoDbOption = "heartbeatFrequencyMS"
+	Tls                    MongoDbOption = "tls"
+	WriteConcern           MongoDbOption = "w"
+	DirectConnection       MongoDbOption = "directConnection"
+)
+
+const (
+	String MongoDbOptionValueType = "string"
+	Integer MongoDbOptionValueType = "integer"
+	Boolean MongoDbOptionValueType = "boolean"
+	StrOrInt MongoDbOptionValueType = "str or int"
+)
+
+var (
+	SourceISEmpty error = errors.New("field source (path to file or conn str to db) is empty")
+
+	MongoDbOptionsTypes = map[MongoDbOption]string
+)
 
 // DataSourceConfig represent source where we can get
 /*
@@ -22,11 +50,18 @@ const ()
  * Here we should have Validator too
  */
 type DataSourceConfig struct {
-	Type    DataSourceType    `json:"type"`
-	Source  string            `json:"source"`
-	Options map[string]string `json:"options"`
+	Type    DataSourceType           `json:"type"`
+	Source  string                   `json:"source"`
+	Options map[MongoDbOption]string `json:"options"`
 }
 
 func (cfg *DataSourceConfig) Validate() error {
+	if len(cfg.Source) == 0 {
+		return SourceISEmpty
+
+	}
+	if cfg.Type == MONGODB {
+		// validate options values ...
+	}
 	return nil
 }
