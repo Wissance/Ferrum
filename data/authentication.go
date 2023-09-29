a101d0b017d5e173590bf251f6e487f22cde0bdc
Michael Ushakov <um.nix.user@gmail.com>
2023-09-29 15:21:42 +0500

    documentation of some functions and structs

--- data/authentication.go
+++ data/authentication.go
@@ -2,10 +2,12 @@ package data
 
 type AuthenticationType int
 
+// ClientIdAndSecrets AuthenticationType represents Confidential Clients
 const (
 	ClientIdAndSecrets AuthenticationType = 1
 )
 
+// Authentication struct for Clients authentication data, for ClientIdAndSecrets Value stores ClientSecret
 type Authentication struct {
 	Type       AuthenticationType
 	Value      string
