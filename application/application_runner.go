107af09bb8ba753b7bd26d84e719c3da6dc2c857
Michael Ushakov <um.nix.user@gmail.com>
2023-10-11 15:08:27 +0500

    one more package logging is ready

--- application/application_runner.go
+++ application/application_runner.go
@@ -1,17 +1,21 @@
 package application
 
 import (
-	"context"
 	"github.com/wissance/Ferrum/logging"
 )
 
-type AppContextBase struct {
+/*type AppContextBase struct {
 	Context context.Context
-}
+}*/
 
+// AppRunner interface that allows to manipulate application
 type AppRunner interface {
+	// Start this function starts initialized application (must be called after Init)
 	Start() (bool, error)
+	// Stop function to stop application
 	Stop() (bool, error)
+	// Init function initializes application components
 	Init() (bool, error)
+	// GetLogger function that required after app initialized all components to log some additional information about application stop
 	GetLogger() *logging.AppLogger
 }
