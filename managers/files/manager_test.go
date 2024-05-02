4911b76ecebceffa3bb19995b7046408ba7e1c18
Michael Ushakov <um.nix.user@gmail.com>
2024-05-02 16:30:11 +0500

    reduced amount ot tests on FileDataManager

--- managers/files/manager_test.go
+++ managers/files/manager_test.go
@@ -44,22 +44,6 @@ func TestGetClientSuccessfully(t *testing.T) {
 	checkClient(t, &expectedClient, c)
 }
 
-func TestGetClientsSuccessfully(t *testing.T) {
-
-}
-
-func TestGetClientsSuccessfullyEmptyRealm(t *testing.T) {
-
-}
-
-func TestGetClientsSuccessfullyNonExistingRealm(t *testing.T) {
-
-}
-
-func TestGetUsersSuccessfully(t *testing.T) {
-
-}
-
 func TestGetUserSuccessfully(t *testing.T) {
 	manager := createTestFileDataManager(t)
 	realm := "myapp"
@@ -116,18 +100,6 @@ func TestGetUserByIdSuccessfully(t *testing.T) {
 	user, err := manager.GetUserById(realm, userId)
 	assert.NoError(t, err)
 	checkUser(t, &expectedUser, &user)
-}
-
-func TestGetUsersSuccessfullyEmptyRealm(t *testing.T) {
-
-}
-
-func TestGetUsersSuccessfullyNonExistingRealm(t *testing.T) {
-
-}
-
-func TestGetClientSuccessfullyNonExistingRealm(t *testing.T) {
-
 }
 
 func createTestFileDataManager(t *testing.T) *FileDataManager {
