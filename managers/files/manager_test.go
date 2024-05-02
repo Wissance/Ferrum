570bffe4b6633256894ad055f075fdb58d17a31f
Michael Ushakov <um.nix.user@gmail.com>
2024-05-02 15:06:27 +0500

    tests on FileDataManager (all empty)

---
+++ managers/files/manager_test.go
@@ -0,0 +1,47 @@
+package files
+
+import "testing"
+
+func TestGetRealmSuccessfully(t *testing.T) {
+
+}
+
+func TestGetClientSuccessfully(t *testing.T) {
+
+}
+
+func TestGetClientsSuccessfully(t *testing.T) {
+
+}
+
+func TestGetClientsSuccessfullyEmptyRealm(t *testing.T) {
+
+}
+
+func TestGetClientsSuccessfullyNonExistingRealm(t *testing.T) {
+
+}
+
+func TestGetUsersSuccessfully(t *testing.T) {
+
+}
+
+func TestGetUserSuccessfully(t *testing.T) {
+
+}
+
+func TestGetUserByIdSuccessfully(t *testing.T) {
+
+}
+
+func TestGetUsersSuccessfullyEmptyRealm(t *testing.T) {
+
+}
+
+func TestGetUsersSuccessfullyNonExistingRealm(t *testing.T) {
+
+}
+
+func TestGetClientSuccessfullyNonExistingRealm(t *testing.T) {
+
+}
