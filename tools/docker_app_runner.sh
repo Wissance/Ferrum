a4939b40317c3b603a4caab643116c4c6d3c70bb
Ushakov Michale <um.nix.user@gmail.com>
2024-06-20 19:29:10 +0500

    -dirt

--- tools/docker_app_runner.sh
+++ tools/docker_app_runner.sh
@@ -1,5 +1,5 @@
-#!/bin/sh1
-# BE careful if you got error "./docker_app_runner.sh: cannot execute: required file not found" it 100% means that you MUST REPLACE ALL LINE ENDINGS \r\n -> \n1
-#TODO(UMV): make Smart insert of initial data1
-./create_wissance_demo_users_docker.sh || true1
-./ferrum --config /app/config_docker_w_redis.json1
+#!/bin/sh
+# BE careful if you got error "./docker_app_runner.sh: cannot execute: required file not found" it 100% means that you MUST REPLACE ALL LINE ENDINGS \r\n -> \n
+#TODO(UMV): make Smart insert of initial data
+./create_wissance_demo_users_docker.sh || true
+./ferrum --config /app/config_docker_w_redis.json
