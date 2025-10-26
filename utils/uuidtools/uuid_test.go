e9f393b1702b520980db634482a2e7a26141b2a7
Ushakov Michale <um.nix.user@gmail.com>
2025-10-26 23:00:06 +0500

    uuid empty method check

--- utils/uuidtools/uuid_test.go
+++ utils/uuidtools/uuid_test.go
@@ -1,7 +1,33 @@
 package uuidtools
 
-import "testing"
+import (
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIsUUIDEmpty(t *testing.T) {
-
+	createdUuid := uuid.New()
+	for name, test := range map[string]struct {
+		identifier      *uuid.UUID
+		expectedIsEmpty bool
+	}{
+		"nil pointer uuid": {
+			identifier:      nil,
+			expectedIsEmpty: true,
+		},
+		"non initialized uuid": {
+			identifier:      &uuid.UUID{},
+			expectedIsEmpty: true,
+		},
+		"created uuid": {
+			identifier:      &createdUuid,
+			expectedIsEmpty: false,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			actualIsEmpty := IsUUIDEmpty(test.identifier)
+			assert.Equal(t, test.expectedIsEmpty, actualIsEmpty)
+		})
+	}
 }
