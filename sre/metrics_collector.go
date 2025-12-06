b9747706256c53d2e86daee79b176f760d69c435
Ushakov Michale <um.nix.user@gmail.com>
2025-12-06 15:35:27 +0500

    added service collector

--- sre/metrics_collector.go
+++ sre/metrics_collector.go
@@ -20,6 +20,7 @@ type MetricsCollector struct {
 	HttpRequestsTotalCount *prometheus.CounterVec
 	// Http requests that were finished with server Error (4xx or 5xx)
 	HttpRequestsErrorCount *prometheus.CounterVec
+	HttpRequestDurations   prometheus.Summary
 	// Request to dataSource (is not usable for File), cli not produces HttpRequestsCount
 	DataSourceRequestsTotalCount *prometheus.CounterVec
 	DataSourceRequestDurations   prometheus.Summary
@@ -27,12 +28,14 @@ type MetricsCollector struct {
 
 func CreateMetricsCollector() *MetricsCollector {
 	mc := MetricsCollector{}
+	// 1. HTTP Section
 	mc.HttpRequestsTotalCount = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
 			Help: "Processed HTTP requests count, partitioned by status",
 		},
 		[]string{Status})
+	prometheus.MustRegister(mc.HttpRequestsTotalCount)
 
 	mc.HttpRequestsErrorCount = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -40,13 +43,29 @@ func CreateMetricsCollector() *MetricsCollector {
 			Help: "Processed HTTP requests errors count, partitioned by error type",
 		},
 		[]string{ErrorType})
+	prometheus.MustRegister(mc.HttpRequestsErrorCount)
 
+	mc.HttpRequestDurations = prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Name:       "http_request_durations",
+			Help:       "Http requests latencies in seconds",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}})
+	prometheus.MustRegister(mc.HttpRequestDurations)
+	// 2. DataSource section
 	mc.DataSourceRequestsTotalCount = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "data_source_requests_total",
 			Help: "Processed Data source requests count, partitioned by status",
 		},
 		[]string{Status})
+	prometheus.MustRegister(mc.DataSourceRequestsTotalCount)
+
+	mc.DataSourceRequestDurations = prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Name:       "http_request_durations",
+			Help:       "Http requests latencies in seconds",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}})
+	prometheus.MustRegister(mc.DataSourceRequestDurations)
 
 	return &mc
 }
