2329dfd34c56ff40ba92b970cd6bcfe8fbf1a224
Ushakov Michale <um.nix.user@gmail.com>
2026-01-06 14:36:46 +0500

    client generation

--- tools/gen_users_for_perf_tests.py
+++ tools/gen_users_for_perf_tests.py
@@ -4,6 +4,9 @@ import argparse
 import datetime
 import typing
 
+default_config = "config_docker_w_redis.json"
+default_client_secret = "00000000000000000000000000000000"
+
 realms_n_arg = "--realms"
 user_per_realm_arg = "--realm_users"
 output_dir_arg = "--output"
@@ -17,7 +20,6 @@ parser.add_argument(output_dir_arg, help="Generating file", required=True, type=
 def main():
     print ("********* Ferrum perf test data generation started ********")
     try:
-        default_config = "config_docker_w_redis.json"
         args = parser.parse_args()
         output_file = args.output
         realms_number = args.realms
@@ -28,6 +30,9 @@ def main():
                 f.write(realm_cmd)
                 f.write("\n")
                 #todo(umv) :create client
+                client, client_cmd = make_create_client_cmd(realm, r, default_config, default_client_secret)
+                f.write(client_cmd)
+                f.write("\n")
                 #todo(umv) :create users
 
         print ("********* Ferrum perf test data generation successfully finished ********")
@@ -40,6 +45,13 @@ def make_create_realm_cmd(realm_number:int, app_config:str) -> tuple[str, str]:
     realm = str.format("realm_{0}", realm_number)
     realm_create_cmd = realm_create_template.format(app_config, realm)
     return realm, realm_create_cmd
+
+def make_create_client_cmd(realm:str, client_number:int, app_config:str, client_secret:str) -> tuple[str, str]:
+    client_create_template = "./ferrum-admin --config={0} --resource=client --operation=create --value=\'{{\"id\": \"{1}\", \"name\": \"{2}\", \"type\": \"confidential\", \"auth\": {{\"type\": 1, \"value\": \"{3}\"}} }}' --params=\"{4}\""
+    client_id = str.format("00000000-0000-0000-0000-{0:012d}", client_number)
+    client = str.format("client_{0}", client_number)
+    client_create_cmd =  client_create_template.format(app_config, client_id, client, client_secret, realm)
+    return client, client_create_cmd
 
 if __name__ == "__main__":
     main()
