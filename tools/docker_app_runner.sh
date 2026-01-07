c513ae2433a350d4adaf0dd74bbb532da89b390a
Ushakov Michale <um.nix.user@gmail.com>
2026-01-07 23:13:22 +0500

    saved data init script in env

--- tools/docker_app_runner.sh
+++ tools/docker_app_runner.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# BE careful if you got error "./docker_app_runner.sh: cannot execute: required file not found" it 100% means that you MUST REPLACE ALL LINE ENDINGS \r\n -> \n
+# BE careful if you got error "./docker_app_runner.sh: cannot execute: required file not found"
+# it 100% means that you MUST REPLACE ALL LINE ENDINGS \r\n -> \n
 #TODO(UMV): make Smart insert of initial data
-/app/tools/create_wissance_demo_users_docker.sh || true
+/app/tools/${FERRUM_DATA_INIT_SCRIPT} || true
 /app/ferrum --config /app/config_docker_w_redis.json $FERRUM_ADDITIONAL_OPTS
