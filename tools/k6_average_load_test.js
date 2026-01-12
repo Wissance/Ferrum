76b48f66ff70582e4572c4e32a105b6cffc6a706
Ushakov Michale <um.nix.user@gmail.com>
2026-01-12 23:52:29 +0500

    1 stage of avg load test

--- tools/k6_average_load_test.js
+++ tools/k6_average_load_test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { check } from "k6";
 import { sleep } from 'k6';
+import encoding from 'k6/encoding';
 export let options = {
     stages: [
         // Ramp-up from 1 to 100 VUs in 1m
@@ -47,39 +48,63 @@ export default function () {
         'Get access token status is 200': (r) => r.status === 200,
     });
     const responseBody = JSON.parse(getTokenResponse.body);
-    let accessToken = responseBody.access_token
+    let accessToken = responseBody.access_token;
+    let refreshToken = responseBody.refresh_token;
     // 3. send up 100 requests userinfo (1-2 sec interval)
     for (let i = 0; i < numOfIterationsStage1; i++) {
         if (i > 0 && i%10 === 0)
         {
-            // send refresh token + introspect
+            // send refresh token
+            let refreshTokenResponse = refreshAccessToken(ferrumBaseUrl, realm, clientId, clientSecret, refreshToken);
+            check(refreshTokenResponse, {
+                'Get access token (refresh) status is 200': (r) => r.status === 200,
+            });
+            const responseBody = JSON.parse(refreshTokenResponse.body);
+            accessToken = responseBody.access_token;
+            refreshToken = responseBody.refresh_token;
+            //
+            let introspectTokenResponse = introspectToken(ferrumBaseUrl, realm, clientId, clientSecret)
+            check(introspectTokenResponse, {
+                'Token introspect status is 200': (r) => r.status === 200,
+            });
         }
         // 3.1 after every 10 request rotate key
-        let getUserInfoResponse = getUserInfo(ferrumBaseUrl, realm, accessToken)
-        check(getUserInfoResponse, {
-            'Get userinfo status is not 500': (r) => r.status !== 500,
-        });
-        if (getUserInfoResponse.status === 401) {
-            getTokenResponse = getAccessToken(ferrumBaseUrl, realm, clientId, clientSecret, user, userPassword)
-            // check status
-            check(getTokenResponse, {
-                'Get access token status is 200': (r) => r.status === 200,
-            });
-            const responseBody = JSON.parse(getTokenResponse.body);
-            accessToken = responseBody.access_token
-        }
-        getUserInfoResponse = getUserInfo(ferrumBaseUrl, realm, accessToken)
-        check(getUserInfoResponse, {
-            'Get userinfo status is 200': (r) => r.status !== 500,
-        });
+        let t = geUserInfoCheck(ferrumBaseUrl, realm, clientId, clientSecret, user,
+            userPassword, accessToken)
+        accessToken = t.accessToken
+        refreshToken = t.refreshToken
         let pause = getRandomInt(1,3)
         sleep(pause)
     }
 
     // 4 wait 2m
-    sleep(120)
+    // sleep(120)
     // 5 send up to 50 requests userinfo (2-3 sec interval)
     // 5.1 after every 10 request rotate key - send refresh token + introspect
+}
+
+function geUserInfoCheck(baseUrl, realm, clientId, secret, username, password, accessToken) {
+    let getUserInfoResponse = getUserInfo(baseUrl, realm, accessToken)
+    let accessTokenNew = null;
+    let refreshTokenNew = null;
+    check(getUserInfoResponse, {
+        'Get userinfo status is not 500': (r) => r.status !== 500,
+    });
+    if (getUserInfoResponse.status === 401) {
+        let getTokenResponse = getAccessToken(baseUrl, realm, clientId, secret, username, password)
+        // check status
+        check(getTokenResponse, {
+            'Get access token status is 200': (r) => r.status === 200,
+        });
+        const responseBody = JSON.parse(getTokenResponse.body);
+        accessTokenNew = responseBody.access_token;
+        refreshTokenNew = responseBody.refresh_token;
+    }
+    getUserInfoResponse = getUserInfo(baseUrl, realm, accessToken)
+    check(getUserInfoResponse, {
+        'Get userinfo status is 200': (r) => r.status !== 500,
+    });
+    return {accessToken: accessTokenNew, refreshToken: refreshTokenNew}
 }
 
 /* This function get access token from Ferrum
@@ -123,12 +148,36 @@ function getUserInfo(baseUrl, realm, accessToken) {
     return http.get(url, params);
 }
 
-function refreshAccessToken() {
+function refreshAccessToken(baseUrl, realm, clientId, secret, refreshToken) {
+    const url = baseUrl+"/auth/realms/" + realm + "/protocol/openid-connect/token";
 
+    var payload = {
+        client_id: clientId,
+        client_secret : secret,
+        grant_type: "refresh_token",
+        refresh_token: refreshToken
+    };
+
+    // k6 automatically handles the encoding and sets the header,
+    // but you can explicitly set it for clarity if needed.
+    const params = {
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded',
+        },
+    };
+
+    return http.post(url, payload, params);
 }
 
-function  introspectToken() {
+function  introspectToken(baseUrl, realm, clientId, secret) {
+    const url = baseUrl+"/auth/realms/" + realm + "/protocol/openid-connect/token/introspect";
+    const credentials = `${clientId}:${secret}`;
+    const encodedCredentials = encoding.b64encode(credentials, 'std', 's');
+    const headers = {
+        "Authorization": `Basic ${encodedCredentials}`,
+    };
 
+    return http.get(url, { headers: headers });
 }
 
 function getRandomInt(min, max) {
