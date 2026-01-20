677dbe39aa3b394a77b2c1a2275736de141d2ae7
Ushakov Michale <um.nix.user@gmail.com>
2026-01-20 13:18:09 +0500

    env switch in perftests

--- start_docker_perftests.ps1
+++ start_docker_perftests.ps1
@@ -1,1 +1,9 @@
-docker-compose -f docker-compose.perf.yml  up --build
+try
+{
+   cp .env.perftests .env
+   docker-compose -f docker-compose.perf.yml  up --build
+}
+finally
+{
+    cp .env.local .env
+}
